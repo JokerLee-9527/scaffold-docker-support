f2150e0ef1818375c4db4e8dbe2eef644eae25a6
JokerLee <lijian79@gmail.com>
2021-09-15 16:54:23 +0800

    run

--- stop_and_start_docker.sh
+++ stop_and_start_docker.sh
@@ -3,7 +3,12 @@ set -e
 
 ./script/docker/stop_docker_services.sh
 
-rm -rf ./docker_data/mysql/db_data/
-rm -rf ./docker_data/redis/data/
+#rm -rf ./docker_data/logstash/tmp/!{.gitkeep}
+#rm -rf ./docker_data/mongodb/bitnami !{.gitkeep}
+#rm -rf ./docker_data/mysql/ !{.gitkeep}
+#rm -rf ./docker_data/rabbitmq/ !{.gitkeep}
+#rm -rf ./docker_data/redis/ !{.gitkeep}
+#rm -rf ./docker_data/zookeeper/ !{.gitkeep}
+
 
 ./script/docker/start_docker_services.sh
--- stop_docker.sh
+++ stop_docker.sh
@@ -2,3 +2,10 @@
 
 ./script/docker/stop_docker_services.sh
 
+#rm -rf ./docker_data/logstash/tmp/ !{.gitkeep}
+#rm -rf ./docker_data/mongodb/bitnami !{.gitkeep}
+#rm -rf ./docker_data/mysql/ !{.gitkeep}
+#rm -rf ./docker_data/rabbitmq/ !{.gitkeep}
+#rm -rf ./docker_data/redis/ !{.gitkeep}
+#rm -rf ./docker_data/zookeeper/ !{.gitkeep}
+
